c3d5580bb1194c6a996f4a3f80ac044859163731
Felipe Oliveira <felpd3v@gmail.com>
2020-04-20 12:54:25 -0300

    v1 Working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markdownSource: ''
+    };
+  }
+
+  handleMarkdownChange = (e) => {
+    localStorage.setItem('markreator', e.target.value);
+    this.setState({
+      markdownSource: e.target.value
+    });
+  }
+
+  componentDidMount() {
+    const defaultValue = `# Markreator\n_simple as it must be!_\n\n***\n\nI Really hope you understand that this is version 1, so if you get some bugs feel free to help us to improve at [Github](https://github.com/felipe0liveira/markreator).\n\n### This version have\n- Auto Save`;
+    this.setState({
+      markdownSource: localStorage.getItem('markreator') || defaultValue
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Markreator</h1>
+          <small>simple as it must be!</small>
+        </header>
+        <section>
+          <textarea onChange={this.handleMarkdownChange} defaultValue={this.state.markdownSource}></textarea>
+          <ReactMarkdown className="preview" source={this.state.markdownSource} />
+        </section>
+      </div>
+    );
+  }
 }
-
-export default App;
